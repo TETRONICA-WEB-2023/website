6bfece5212fa1f27fb6de9ecec213e0047737bc5
runs664 <harunsixsixfour@gmail.com>
2023-10-24 20:52:27 +0700

    add dummy string

--- src/component/firebasecon.tsx
+++ src/component/firebasecon.tsx
@@ -1,14 +1,14 @@
 import { initializeApp, getApps } from "firebase/app";
 
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || 'dummy',
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || 'dummy',
+    databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL || 'dummy',
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || 'dummy',
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || 'dummy',
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || 'dummy',
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || 'dummy',
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID || 'dummy',
 };
 
 const app = initializeApp(firebaseConfig);
