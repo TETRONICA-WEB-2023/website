539eb9b71d52199314b3eba8886fdb5daa428856
faiqpr <faiqpramatya@gmail.com>
2023-11-17 10:55:18 +0700

    add thankyou page

--- app/thankyou/page.jsx
+++ app/thankyou/page.jsx
@@ -1,7 +1,20 @@
-export default function thankYou () {
+import React from "react";
+import "./thankyou.css";
+import Link from "next/link";
+
+export default function thankYou() {
   return (
-    <div>
-      <h1>Thank You</h1>
+    <div className="thankyou-container">
+      <div className="outer-border">
+        <div className="inner-border">
+          <div className="headings">
+            <h1 className="h-top">TERIMAKASIH TELAH BERPARTISIPASI DALAM</h1>
+            <h3 className="h-bottom">PEMILIHAN UMUM KMTETI 2023</h3>
+          </div>
+          <img className="logo-center" src="/tetronica.svg" alt="logo" />
+          <Link href='/' className="home-btn">Kembali ke Beranda</Link>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
